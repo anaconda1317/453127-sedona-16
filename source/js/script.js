04f284643065ba415531801265b543c7c040e8ef
kate <kate.klimanovski@gmail.com>
2019-05-24 20:08:37 +0300

    часть правок с первой проверки

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,13 @@
-      // Класс  main-nav__toggle- -closed по умолчанию,- мобильное меню закрыто, для его
-      // открытия нужно нажать на кнопку бургера, для закрытия тоже нажать на крестик
-      // Переменные для открытия и закрытия верхнего меню в мобилке
+// Класс  main-nav__toggle- -closed по умолчанию,- мобильное меню закрыто, для его
+// открытия нужно нажать на кнопку бургера, для закрытия тоже нажать на крестик
+// Переменные для открытия и закрытия верхнего меню в мобилке
 
 var mainNavToggle = document.querySelector(".main-nav__toggle");
 var listToggle = document.querySelector(".main-nav__list");
 var logoToggle = document.querySelector(".page-header__logo");
 
 // действия по умолчанию -Если есть JS, то лишние классы ниже убираются,
- // добавляется -конкретно класс закрытого меню - и у нас грузится страница, со спрятанным меню
+// добавляется -конкретно класс закрытого меню - и у нас грузится страница, со спрятанным меню
 
 mainNavToggle.classList.remove("main-nav__toggle--nojs");
 mainNavToggle.classList.remove("main-nav__toggle--closed");
